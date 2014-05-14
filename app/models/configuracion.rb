d8bda159bb2760f19dc8448836b362c311d93248
Leonardo Arias Paz <leoariaspaz@gmail.com>
2014-05-14 13:01:55 -0300

    Se mueve scope :current a un método self.current

--- app/models/configuracion.rb
+++ app/models/configuracion.rb
@@ -1,7 +1,10 @@
 class Configuracion < ActiveRecord::Base
 	before_save :set_fecha_proceso_sin_hora
 
-	scope :current, where(id: 1).first
+	def self.current
+	  where(id: 1).first
+	end
+	
 private
 	def set_fecha_proceso_sin_hora
 		fecha_proceso = fecha_proceso.to_s
